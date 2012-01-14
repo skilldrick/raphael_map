dbcdf16e85a19a7b03cfb3b4028221f2cece05dd
Nick Morgan <nickm@twitter.com>
2012-01-13 22:14:55 -0800

    Draw shapes with the mouse.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,54 @@
 $(document).ready(function () {
   var width = 400;
   var height = 400;
+  var closeTolerance = 5;
   var paper = Raphael('map', width, height);
   var border = paper.rect(0, 0, 400, 400);
   border.attr('stroke', 'black');
+  var currentPath = null;
+  var shapes = [];
 
   $('#map').click(function (e) {
     var x = e.offsetX, y = e.offsetY;
-    paper.circle(x, y, 1);
+
+    if (!currentPath) {
+      currentPath = newPath(x, y);
+    } else if (currentPath.shouldClose(x, y)) {
+      currentPath.closePath();
+      shapes.push(currentPath.path);
+      console.log(shapes);
+      currentPath = null;
+    } else {
+      currentPath.addPoint(x, y);
+    }
   });
+
+  Raphael.el.addPart = function (point) {
+    if (this.type != 'path') {
+      throw new TypeError('addPart can only be used on paths');
+    }
+    var pathParts = this.attr('path') || [];
+    pathParts.push(point);
+    this.attr('path', pathParts);
+    return this;
+  };
+
+  function newPath(startX, startY) {
+    var path = paper.path().addPart(['M', startX, startY]);
+
+    return {
+      path: path,
+      addPoint: function (x, y) {
+        path.addPart(['L', x, y]);
+      },
+      closePath: function () {
+        path.addPart(['Z']);
+      },
+      shouldClose: function (x, y) {
+        return Math.abs(startX - x) < closeTolerance &&
+          Math.abs(startY - y) < closeTolerance;
+      }
+    };
+  }
 });
+
